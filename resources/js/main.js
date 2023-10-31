8e8c5d7fa3afab638b7842d3a2613305c6fc9be2
dueselder <c.dueselder@live.de>
2023-10-31 21:46:57 +0100

    smartphone adjustments

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,4 @@
+// Smooth Scroll
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -7,23 +8,30 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
-
+// Sticky Navbar
 const navbar = document.querySelector('nav');
-
 window.addEventListener('scroll', function() {
-    if(window.scrollY > 700) { 
+    if(window.scrollY > 300) { // Adjusted value for mobile screens
         navbar.classList.add('sticky');
     } else {
         navbar.classList.remove('sticky');
     }
 });
 
-
+// Navbar Toggle
 document.getElementById("navToggle").addEventListener("click", function() {
     const navItems = document.getElementById("navItems");
     navItems.classList.toggle('show');
 });
 
+// Close navbar when a nav item is clicked on mobile
+document.querySelectorAll('#navItems a').forEach(item => {
+    item.addEventListener('click', () => {
+        document.getElementById("navItems").classList.remove('show');
+    });
+});
+
+// Loading Bars
 document.addEventListener("DOMContentLoaded", function() {
     const fills = document.querySelectorAll(".fill");
     fills.forEach(fill => {
